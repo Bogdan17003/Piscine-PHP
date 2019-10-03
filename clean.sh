569a820eaa64e61446f72600612416fc94c29ab0
Bogdan DEOMIN <bdeomin@e1r5p14.unit.ua>
2019-10-04 00:11:16 +0300

    clean.sh

---
+++ clean.sh
@@ -0,0 +1,3 @@
+find . -name ".DS_Store" -print -delete -or -name "*~" -print -delete -or -name "#*#" -print  -delete -or -name "a.out" -print -delete
+find . -name ".idea" -print -delete
+find . -name "clean.sh" -print -delete
